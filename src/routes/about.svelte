035ee4b79aa537c0195e051c0d9708245a0e8c1c
Jun-Hee Lee <flyingmt@gmail.com>
2022-04-10 19:25:30 +0900

    first commit

---
+++ src/routes/about.svelte
@@ -0,0 +1,19 @@
+<script>
+
+</script>
+
+<div class="about">
+    <h2>About Us</h2>
+    <p>aksdjfhaklsdjfh askdjhflaksdjf aksdjdhflk askdjhfasd klasjhdflkjahsdf 
+        asdfk jhasdf kljhasdf asdfhkkj asdkjfh asdkfh askdfjh askdfh askdfh asdkfh</p>
+    <a href="/guides">View Guides</a>
+    <a href="/">Home</a>
+</div>
+
+<style>
+    .about {
+        text-align: center;
+        display: block;
+        margin: 20px auto;
+    }
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,19 @@
+<script>
+
+</script>
+
+<div class="index">
+    <h2>Welcome</h2>
+    <p>aksdjfhaklsdjfh askdjhflaksdjf aksdjdhflk askdjhfasd klasjhdflkjahsdf 
+    asdfk jhasdf kljhasdf asdfhkkj asdkjfh asdkfh askdfjh askdfh askdfh asdkfh</p>
+    <a href="/guides">View Guides</a>
+    <a href="/about">About</a>
+</div>
+
+<style>
+    .index {
+        text-align: center;
+        display: block;
+        margin: 20px auto;
+    }
+</style>
